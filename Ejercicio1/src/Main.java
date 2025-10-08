7c1cf1be3608ede29c3b7ed28f89f504b0041529
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-08 10:31:05 +0200

    fix: change in Main class.
    We call de methods directly from the class Entry not by an instance of it.

--- Ejercicio1/src/Main.java
+++ Ejercicio1/src/Main.java
@@ -1,25 +1,14 @@
-/*
-        public static byte leerByte(String mensaje);
-        public static int leerInt(String mensaje);
-        public static float leerFloat(String mensaje);
-        public static double leerDouble(String mensaje);
-        public static char leerChar(String mensaje);
-        public static String leerString(String mensaje);
-        public static boolean leerSiNo(String mensaje), si el usuario introduce “s”, devuelve “true”, si el usuario introduce “n”, devuelve “false”.
-*/
-
 public class Main {
     public static void main(String[] args) {
 
-        Entry entrada1 = new Entry();
 
-        //entrada1.leerByte("Introduce tu edad");
-        //entrada1.leerInt("Introduce tu año de nacimiento");
-        //entrada1.leerFloat("Introduce un número con coma");
-        // entrada1.leerDouble("Introduce un número con coma");
-        //entrada1.leerChar("Introduce un único carácter");
-        //entrada1.leerString("Introduce una palabra");
-        if (entrada1.leerSiNo("Te gusta los parsteles? Responde s: sí o n: no")) System.out.println("ok te mola");
+        Entry.leerByte("Introduce tu edad");
+        Entry.leerInt("Introduce tu año de nacimiento");
+        Entry.leerFloat("Introduce un número con coma");
+        Entry.leerDouble("Introduce un número con coma");
+        Entry.leerChar("Introduce un único carácter");
+        Entry.leerString("Introduce una palabra");
+        if (Entry.leerSiNo("Te gusta los parsteles? Responde s: sí o n: no")) System.out.println("ok te mola");
 
 
     }
