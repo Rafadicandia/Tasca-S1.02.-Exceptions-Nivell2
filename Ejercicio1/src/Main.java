728a98429ec7d7bb72c58e734ad198639f0824df
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-08 11:03:54 +0200

    test: no bother

--- Ejercicio1/src/Main.java
+++ Ejercicio1/src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Entry.readByte("How old are you?");
-        Entry.readInt("Which year where you born?");
-        Entry.readFloat("Type in a number with comma");
+       Entry.readByte("How old are you?");
+      Entry.readInt("Which year where you born?");
+       Entry.readFloat("Type in a number with comma");
         Entry.readDouble("Type in a number with comma");
         Entry.readChar("Type in a single character");
         Entry.readString("Type in a word");
-        Entry.readYesNo("Do you like cake? Answer Y: yes or N: no");
+       Entry.readYesNo("Do you like cake? Answer Y: yes or N: no");
 
 
     }
